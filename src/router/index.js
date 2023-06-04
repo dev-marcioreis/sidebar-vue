fb6fa26921be6c6c135662e7b77b1b27c32ee8a5
Márcio Reis <122680054+dev-marcioreis@users.noreply.github.com>
2023-06-04 10:32:11 -0300

    testing router

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ const router = createRouter({
     history: createWebHistory(),
     routes: [
         {
-            path: '/sidebar-vue/',
+            path: '/',
             component: Home
         },
         {
